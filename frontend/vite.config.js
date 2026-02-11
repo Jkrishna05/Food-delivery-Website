68f3be82c3f547b01d22d0fd7ad9263c733ed184
Jkrishna05 <gourimanirout@gmail.com>
2026-02-11 19:46:15 +0530

    bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  },
+  server: {
+    middlewareMode: false,
+  }
 })
